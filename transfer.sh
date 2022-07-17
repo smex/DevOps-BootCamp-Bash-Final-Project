f50365cc236ec3c369aa2b86324d981b861e0e47
smex <cnexxx@gmail.com>
2022-07-17 23:35:37 +0300

    Reformat code

--- transfer.sh
+++ transfer.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-USAGE=$(cat <<EOF
+USAGE=$(
+  cat <<EOF
 Description: Bash tool to transfer files from the command line.
 Usage: transfer.sh [-d | -h | -v] [file1 ...]
   -d  dir_name file_id file_name  Download file
@@ -16,48 +17,50 @@ EOF
 
 currentVersion="1.23.0"
 
-httpSingleUpload()
-{
-    response=$(curl -A curl --upload-file "$1" "https://transfer.sh/$2") || { echo "Failure!"; return 1;}
+httpSingleUpload() {
+  response=$(curl -A curl --upload-file "$1" "https://transfer.sh/$2") || {
+    echo "Failure!"
+    return 1
+  }
 }
 
-printUploadResponse()
-{
-fileID=$(echo "$response" | cut -d "/" -f 4)
+printUploadResponse() {
+  fileID=$(echo "$response" | cut -d "/" -f 4)
   cat <<EOF
 Transfer File URL: $response
 EOF
 }
 
-singleUpload()
-{
+singleUpload() {
   filePath=$(echo "$1" | sed s:"~":"$HOME":g)
-  if [ ! -f "$filePath" ]; then { echo "Error: invalid file path"; return 1;}; fi
+  if [ ! -f "$filePath" ]; then {
+    echo "Error: invalid file path"
+    return 1
+  }; fi
   tempFileName=$(echo "$1" | sed "s/.*\///")
   echo "Uploading $tempFileName"
   httpSingleUpload "$filePath" "$tempFileName"
 }
 
-singleDownload()
-{
+singleDownload() {
   echo "Downloading to $2 from $1"
   curl --progress-bar "$1" -o "$2"
 }
 
 getopts "dhv" opt
 case "$opt" in
-  h)
-    echo "$USAGE" && exit
-    ;;
-  v)
-    echo $currentVersion && exit
-    ;;
-  d)
-    url="https://transfer.sh/$3/$4"
-    file="$2$4"
-    singleDownload "$url" "$file"
-    exit
-    ;;
+h)
+  echo "$USAGE" && exit
+  ;;
+v)
+  echo $currentVersion && exit
+  ;;
+d)
+  url="https://transfer.sh/$3/$4"
+  file="$2$4"
+  singleDownload "$url" "$file"
+  exit
+  ;;
 esac
 
 for file in $@; do
